575a4c4f26015054a0512bebe02aadc0794a9a29
Jaehwa Han <drh@wafflestudio.com>
2014-02-16 14:14:16 +0900

    working on password reset function

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,19 @@ Ceotalk::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "wafflestudio.com",
+    user_name: "drh@wafflestudio.com",
+    password: ENV["GMAIL_SMTP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = {host: "ceotalk.dev"}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
