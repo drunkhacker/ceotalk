6c9feb9b1e57d0e72327869e5bd41481b465f2c9
Jaehwa Han <drh@wafflestudio.com>
2014-02-27 01:27:43 +0900

    ..

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,8 +1,16 @@
 class ProfessionalsController < ApplicationController
   respond_to :html, :js
   def index
-    @professionals = Professional.page(params[:page]).per(20)
     @indexes = %w(ㄱ ㄴ ㄷ ㄹ ㅁ ㅂ ㅅ ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ)
+    offsets = %w(가 깋 나 닣 다 딯 라 맇 마 밓 바 빟 사 싷 아 잏 자 짛 차 칳 카 킿 타 팋 파 핗 하 힣)
+
+    @name_filter = unless params[:name_filter].nil? then params[:name_filter].to_i end
+    if params[:name_filter] && params[:name_filter].to_i >= 0
+      idx = params[:name_filter].to_i
+      @professionals = Professional.where("name >= ? AND name <= ?", offsets[idx*2], offsets[idx*2+1]).page(params[:page]).per(20)
+    else
+      @professionals = Professional.page(params[:page]).per(20)
+    end
   end
 
   def show
