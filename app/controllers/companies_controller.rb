747babb8d9b116725e60fd2cfc8e6308b84ae65a
Jaehwa Han <drh@wafflestudio.com>
2014-03-02 14:55:29 +0900

    ..

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,12 +4,14 @@ class CompaniesController < ApplicationController
     @indexes = %w(ㄱ ㄴ ㄷ ㄹ ㅁ ㅂ ㅅ ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ)
     offsets = %w(가 깋 나 닣 다 딯 라 맇 마 밓 바 빟 사 싷 아 잏 자 짛 차 칳 카 킿 타 팋 파 핗 하 힣)
 
-    @name_filter = unless params[:name_filter].nil? then params[:name_filter].to_i end
-    if params[:name_filter] && params[:name_filter].to_i >= 0
-      idx = params[:name_filter].to_i
-      @companies = Company.where("name >= ? AND name <= ?", offsets[idx*2], offsets[idx*2+1]).page(params[:page]).per(20)
-    else
+    @name_filter = if params[:name_filter] && params[:name_filter].length > 0 then params[:name_filter].to_i else -1 end
+    logger.debug "@name_filter = #{@name_filter}"
+    if @name_filter >= 0 and @name_filter < @indexes.length
+      @companies = Company.where("name >= ? AND name <= ?", offsets[@name_filter*2], offsets[@name_filter*2+1]).page(params[:page]).per(20)
+    elsif @name_filter < 0
       @companies = Company.page(params[:page]).per(20)
+    else #기타
+      @companies = Company.where("name < ? OR name > ?", offsets.first, offsets.last).page(params[:page]).per(20)
     end
   end
 
