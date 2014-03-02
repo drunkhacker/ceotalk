4ec07792d1ed2d665d498812d5a4d4f823e6d788
Jaehwa Han <drh@wafflestudio.com>
2014-03-03 02:46:40 +0900

    ..

--- lib/video_embed.rb
+++ lib/video_embed.rb
@@ -1,6 +1,6 @@
 module VideoEmbeddable
   def embed_url(field=:url)
-    pattern_youtube = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9]+)/i
+    pattern_youtube = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_]+)/i
     r = self.send(field).scan pattern_youtube
 
     unless r.empty?
@@ -8,7 +8,7 @@ module VideoEmbeddable
       return "//www.youtube.com/embed/#{vid}"
     end
 
-    pattern_vimeo = /(?:https?:\/\/)?(?:www\.)?vimeo\.com\/([a-zA-Z0-9]+)/i
+    pattern_vimeo = /(?:https?:\/\/)?(?:www\.)?vimeo\.com\/([a-zA-Z0-9_]+)/i
     r = self.send(field).scan pattern_vimeo
 
     unless r.empty?
