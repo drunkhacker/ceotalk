accd5ed1827f7f4ba8b2ed83d823bc4d3d9b1a7b
Jaehwa Han <drh@wafflestudio.com>
2014-03-02 13:36:08 +0900

    ...

--- config/initializers/ellipsisize.rb
+++ config/initializers/ellipsisize.rb
@@ -1,4 +1,14 @@
 class String
+  def truncate_center(target_length=16)
+    return self if self.length <= target_length
+    p_len = (target_length - 2)/2
+
+    head = self[0...p_len]
+    tail = self[self.length-p_len-1...-1]
+
+    "#{head}...#{tail}"
+  end
+
   def ellipsisize(minimum_length=4,edge_length=3)
     return self if self.length < minimum_length or self.length <= edge_length*2 
     edge = '.'*edge_length    
