1deebb558d07244c6cf6e1f0a54d7bbadebbb0aa
Jaehwa Han <drh@wafflestudio.com>
2014-03-04 10:25:18 +0900

    ..

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,5 +1,5 @@
 class CommentMailer < ActionMailer::Base
-  default from: "drh@wafflestudio.com"
+  default from: '"CEO MBA" <ask@ceomba.co.kr>'
 
   def talk_comments_notify_email(user, videos_and_comments, posts_and_comments, user_comments)
     @user = user
@@ -9,6 +9,6 @@ class CommentMailer < ActionMailer::Base
 
     logger.debug"@user_comments.length = #{@user_comments.length}"
 
-    mail(to: @user.email, subject: "CEO MBA #{@user.name}님의 컨텐츠에 새롭게 달린 댓글입니다!")
+    mail(to: @user.email, subject: "CEO MBA | (#{Time.now.strftime("%y.%m.%d")})#{@user.name}님의 컨텐츠의 새로운 댓글을 알려드립니다.")
   end
 end
